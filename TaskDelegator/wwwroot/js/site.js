171e121fc55924bb23c4de38f830170a7a111e88
Filip Wilczura <filip.wilczura@microsoft.wsei.edu.pl>
2018-11-01 21:05:35 +0100

    Added Dragula (drag and drop) library

--- TaskDelegator/wwwroot/js/site.js
+++ TaskDelegator/wwwroot/js/site.js
@@ -110,3 +110,34 @@ function createListElement(tasksPoolList, task)
     container.appendChild(textNode);
     tasksPoolList.appendChild(container);
 }
+
+let dragulaOptions = {
+    isContainer: function (el) {
+        return false; // only elements in drake.containers will be taken into account
+    },
+    moves: function (el, source, handle, sibling) {
+        return true; // elements are always draggable by default
+    },
+    accepts: function (el, target, source, sibling) {
+        return true; // elements can be dropped in any of the `containers` by default
+    },
+    invalid: function (el, handle) {
+        return false; // don't prevent any drags from initiating by default
+    },
+    direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
+    copy: false,                       // elements are moved by default, not copied
+    copySortSource: false,             // elements in copy-source containers can be reordered
+    revertOnSpill: true,              // spilling will put the element back where it was dragged from, if this is true
+    removeOnSpill: false,              // spilling will `.remove` the element, if this is true
+    mirrorContainer: document.body,    // set the element that gets mirror elements appended
+    ignoreInputTextSelection: true     // allows users to select input text, see details below
+};
+
+//Containers for dragula (drag and drop library) which allows dragging items between them
+dragula([
+    document.querySelector('#tasks-pool-list'),
+    document.querySelector('#tasks-pool-list-user-one'),
+    document.querySelector('#tasks-pool-list-user-two'),
+    document.querySelector('#tasks-pool-list-user-three')],
+    dragulaOptions
+);
